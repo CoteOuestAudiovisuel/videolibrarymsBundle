17d80a5f8001150c8a2cd909514180133c1c67fd
senateurritchie <zakeszako@yahoo.fr>
2022-01-21 17:08:10 +0000

    creation des services du bundle

--- src/VideolibraryBundle.php
+++ src/VideolibraryBundle.php
@@ -5,5 +5,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class VideolibraryBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 
 }
