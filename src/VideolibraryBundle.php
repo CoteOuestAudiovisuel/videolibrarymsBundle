06578ca71572c383aeb5fa967b32dff38b90fc7f
senateurritchie <zakeszako@yahoo.fr>
2022-01-21 17:56:35 +0000

    rendre le bundle configurable

--- src/VideolibraryBundle.php
+++ src/VideolibraryBundle.php
@@ -1,10 +1,19 @@
 <?php
 namespace Coa\VideolibraryBundle;
+use Coa\VideolibraryBundle\DependencyInjection\VideolibraryExtension;
+
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class VideolibraryBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $ext = new VideolibraryExtension([],$container);
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
