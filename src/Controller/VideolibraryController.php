432630c05f4acc8dc40e6774947b205709eca4e5
senateurritchie <zakeszako@yahoo.fr>
2022-01-22 11:08:08 +0000

    installation des vues v13

--- src/Controller/VideolibraryController.php
+++ src/Controller/VideolibraryController.php
@@ -56,7 +56,7 @@ class VideolibraryController extends AbstractController
 
         if($request->isXmlHttpRequest()){
             $acceptHeader = AcceptHeader::fromString($request->headers->get('Accept'));
-            $view = '@coa_videolibrary/home/item-render.html.twig';
+            $view = '@CoaVideolibrary/home/item-render.html.twig';
         }
 
         return $this->render($view, [
@@ -263,7 +263,7 @@ class VideolibraryController extends AbstractController
                             @unlink($filename);
                         }
                     }
-                    $job["html"] = $this->renderView("@coa_videolibrary/home/item-render.html.twig",["videos"=>[$video]]);
+                    $job["html"] = $this->renderView("@CoaVideolibrary/home/item-render.html.twig",["videos"=>[$video]]);
                 }
             }
             unset($job);
@@ -340,7 +340,7 @@ class VideolibraryController extends AbstractController
                 $video->setJobRef($job["data"]["id"]);
                 $video->setBucket($bucket);
                 $video->setJobPercent(0);
-                $result["html"] = $this->renderView("@coa_videolibrary/home/item-render.html.twig",["videos"=>[$video]]);
+                $result["html"] = $this->renderView("@CoaVideolibrary/home/item-render.html.twig",["videos"=>[$video]]);
             }
 
             $em->persist($video);
