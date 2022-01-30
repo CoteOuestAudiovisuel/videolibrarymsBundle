4a97110f841de47792057b848e009aa5774f109f
senateurritchie <zakeszako@yahoo.fr>
2022-01-30 14:22:50 +0000

    refactor:(command) update cron trash migration

--- src/Command/CoaVideolibraryCommand.php
+++ src/Command/CoaVideolibraryCommand.php
@@ -2,7 +2,6 @@
 
 namespace Coa\VideolibraryBundle\Command;
 
-use Coa\VideolibraryBundle\Entity\Video;
 use Coa\VideolibraryBundle\Service\MediaConvertService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -11,6 +10,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Asset\Packages;
+
 
 class CoaVideolibraryCommand extends Command
 {
@@ -18,17 +20,28 @@ class CoaVideolibraryCommand extends Command
     protected static $defaultDescription = 'commande de transcodage des fichiers uploadés';
     private EntityManagerInterface $em;
     private MediaConvertService $mediaConvert;
+    private ContainerInterface $container;
+    private Packages $packages;
 
-    public function __construct(string $name = null,EntityManagerInterface $em, MediaConvertService $mediaConvert)
+    public function __construct(
+        string $name = null,
+        EntityManagerInterface $em,
+        MediaConvertService $mediaConvert,
+        ContainerInterface $container,
+        Packages $packages
+    )
     {
         parent::__construct($name);
         $this->em = $em;
         $this->mediaConvert = $mediaConvert;
+        $this->container = $container;
+        $this->packages = $packages;
     }
 
     protected function configure(): void
     {
         $this
+            ->addOption('hls-key-baseurl', null, InputOption::VALUE_REQUIRED, "l'url pour les clés DRM")
             ->addOption('sync', null, InputOption::VALUE_NONE, 'synchronise le dossier de depot')
             ->addOption('transcode', null, InputOption::VALUE_NONE, 'lance le trancodage')
         ;
@@ -39,14 +52,13 @@ class CoaVideolibraryCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $isTranscoding = filter_var($input->getOption('transcode'), FILTER_VALIDATE_BOOLEAN);
         $io->title("Gestion videos");
-        $rep = $this->em->getRepository(Video::class);
-        $videosPath = __DIR__."/../../public/coa_videolibrary";
+        $video_entity = $this->container->getParameter('coa_videolibrary.video_entity');
+        $rep = $this->em->getRepository($video_entity);
+        $videosPath = $this->container->getParameter('kernel.project_dir') . "/public/coa_videolibrary";
 
         if(!file_exists($videosPath)){
             mkdir($videosPath);
         }
-
-        //dd($input->getOption('sync'));
 
         // creation des entités video a partir des depots de fichiers dans le dossier de synchronisation
         if($input->getOption('sync')){
@@ -60,7 +72,7 @@ class CoaVideolibraryCommand extends Command
                     $file_length = filesize($filename);
                     $newFilename = $basedir . "/" . $code . ".mp4";
 
-                    $video = new Video();
+                    $video = new $video_entity();
                     $video->setCode($code);
                     $video->setOriginalFilename($basename);
                     $video->setFileSize($file_length);
@@ -78,34 +90,33 @@ class CoaVideolibraryCommand extends Command
         // lance le transcodage
         if($input->getOption("transcode")){
             if(($data = $rep->findBy(["state"=>"pending"],["id"=>"ASC"],10))){
+                $baseurl = $input->getOption("hls-key-baseurl");
                 foreach ($data as $i=>$el){
+                    $code = $el->getCode();
+                    $io->info(sprintf("fichier %s, code: %s, envoyé en transcodage",$el->getOriginalFilename(), $el->getCode()));
+                    $inputfile = $baseurl.$this->packages->getUrl('/coa_videolibrary/'.$code.'.mp4');
+                    $keyfilename = $code;
+                    $bucket = $this->container->getParameter("coa_videolibrary.s3_bucket");
+                    $region = $this->container->getParameter("coa_videolibrary.aws_region");
+                    $keyurl = $baseurl.$this->container->getParameter("coa_videolibrary.keys_route") . "/" . $keyfilename;
 
-//                    $io->info(sprintf("fichier %s, code: %s, envoyé en transcodage",$basename, $code));
-//
-//                    $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath();
-//                    $baseurl = "https://kiwi.loca.lt";
-//
-//                    // c'est ici qu'on lance le processus de transcodage sur AWS
-//                    $inputfile = $baseurl.$packages->getUrl('/coa_videolibrary/'.$code.'.mp4');
-//                    $keyfilename = $code;
-//                    $bucket = $this->getParameter("coa_videolibrary.s3_bucket");
-//                    $keyurl = $baseurl.$this->getParameter("coa_videolibrary.keys_route") . "/" . $keyfilename;
-//
-//                    $result['inputfile'] = $inputfile;
-//                    try {
-//                        $job = $mediaConvert->createJob($inputfile,$keyfilename,$keyurl,$bucket);
-//                        $video->setJobRef($job["data"]["id"]);
-//                        $video->setState("SUBMITTED");
-//                    }catch (\Exception $e){
-//                        throw $e;
-//                    }
-//
-//                    $video->setBucket($bucket);
-//                    $video->setJobPercent(0);
+                    try {
+                        $job = $this->mediaConvert->createJob($inputfile,$keyfilename,$keyurl,$bucket);
+                        $video->setJobRef($job["data"]["id"]);
+                        $video->setState("SUBMITTED");
+                    }catch (\Exception $e){
+                        throw $e;
+                    }
+
+                    $video->setBucket($bucket);
+                    $video->setRegion($region);
+                    $video->setJobPercent(0);
+
+                    $this->em->persist($video);
                 }
+                $this->em->flush();
             }
         }
-
         return Command::SUCCESS;
     }
 }
