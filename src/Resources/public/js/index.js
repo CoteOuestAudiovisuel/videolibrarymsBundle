bb936a556d7978717197c3e8c8fcc3f5f0d36b8e
senateurritchie <zakeszako@yahoo.fr>
2022-04-26 11:33:51 +0000

    remove video sprites

--- src/Resources/public/js/index.js
+++ src/Resources/public/js/index.js
@@ -1,13 +1,13 @@
 var vl = new Aaz.VideoLibrary();
 vl.controller();
 
-window.addEventListener("load",function(){
-    var vs = new Aaz.VideoSprites();
-    vs.init(".video-sprites");
-
-    vl.subscribe(event=>{
-        if(event.type == "newfeed"){
-            vs.init(".video-sprites:not(.video-sprites-item)");
-        }
-    })
-})
+// window.addEventListener("load",function(){
+//     var vs = new Aaz.VideoSprites();
+//     vs.init(".video-sprites");
+//
+//     vl.subscribe(event=>{
+//         if(event.type == "newfeed"){
+//             vs.init(".video-sprites:not(.video-sprites-item)");
+//         }
+//     })
+// })
