a538c9a7b786fee051b9daf1934ad4d3f013dcf5
senateurritchie <zakeszako@yahoo.fr>
2022-05-02 09:27:56 +0000

    avoid cdn on assets

---
+++ src/EventListener/GlobalRequestListener.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\EventListener;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\UrlPackage;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
+
+class GlobalRequestListener
+{
+    private $requestStack;
+    private $packages;
+
+    public function __construct(Packages $packages, RequestStack $requestStack)
+    {
+        $this->packages = $packages;
+        $this->requestStack = $requestStack;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        if ($event->isMainRequest()) {
+            $versionStrategy = new EmptyVersionStrategy();
+            $base_url_cdn = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost();
+            $package = new UrlPackage($base_url_cdn, $versionStrategy);
+            $this->packages->addPackage("coa_videolibrary_host", $package);
+        }
+    }
+}
